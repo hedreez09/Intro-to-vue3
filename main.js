5801353912e5a89d7378f03f24b1256b928e0016
Idris Wonuola <idrisolawonuola@gmail.com>
2024-05-14 19:05:23 -0400

    Use of stlye binding to bind the add to cart button and socks color

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const app = Vue.createApp({
         return{
             cart:0,
             product:'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_green.jpg',
             url: 'https://github.com/hedreez09/',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants:[
                 {id: 2234, color: 'green',image: './assets/images/socks_green.jpg' },
