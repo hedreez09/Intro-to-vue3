0bab09df542ca76bc95f93e74a0bc68f9503e32b
Idris Wonuola <idrisolawonuola@gmail.com>
2024-05-14 19:47:20 -0400

    Use of computed property to manage the in quantity of stock available and their images

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const app = Vue.createApp({
         return{
             cart:0,
             product:'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://github.com/hedreez09/',
-            inStock: false,
+            brand: 'Vue Learning',
+            selectedVariant: 0,
+            onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants:[
-                {id: 2234, color: 'green',image: './assets/images/socks_green.jpg' },
-                {id: 2235, color: 'blue',image: './assets/images/socks_blue.jpg'}
+                {id: 2234, color: 'green',image: './assets/images/socks_green.jpg', quantity: 50 },
+                {id: 2235, color: 'blue',image: './assets/images/socks_blue.jpg', quantity: 0 }
             ]
         }
     },
@@ -18,9 +17,27 @@ const app = Vue.createApp({
         addToCart(){
             this.cart += 1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
+            console.log(index)
         } 
+    },
+    computed:{
+        title(){
+            return this.brand +' '+ this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ' '
+        }
     }
     
 })
